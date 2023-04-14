8720dd6f25482ce19539361d5bce1120c98bcd4a
Karl Bateman <karl.bateman@pm.me>
2023-04-14 16:11:47 +0100

    feat: add unique ids node

---
+++ src/bin/unique-ids.rs
@@ -0,0 +1,70 @@
+use nautilus::*;
+
+use anyhow::{bail, Context};
+use serde::{Deserialize, Serialize};
+use std::io::{StdoutLock, Write};
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(tag = "type")]
+#[serde(rename_all = "snake_case")]
+enum Payload {
+    Generate,
+    GenerateOk {
+        #[serde(rename = "id")]
+        guid: String,
+    },
+    Init {
+        node_id: String,
+        node_ids: Vec<String>,
+    },
+    InitOk,
+}
+
+struct UniqueNode {
+    id: usize,
+}
+
+impl Node<Payload> for UniqueNode {
+    fn step(&mut self, input: Message<Payload>, output: &mut StdoutLock) -> anyhow::Result<()> {
+        match input.body.payload {
+            Payload::Init { .. } => {
+                let reply = Message {
+                    src: input.dst,
+                    dst: input.src,
+                    body: Body {
+                        id: Some(self.id),
+                        in_reply_to: input.body.id,
+                        payload: Payload::InitOk,
+                    },
+                };
+                serde_json::to_writer(&mut *output, &reply)
+                    .context("serialize response to init")?;
+                output.write_all(b"\n").context("write trailing newline")?;
+                self.id += 1;
+            }
+            Payload::InitOk { .. } => bail!("received init_ok message"),
+            Payload::Generate => {
+                let guid = ulid::Ulid::new().to_string();
+                let reply = Message {
+                    src: input.dst,
+                    dst: input.src,
+                    body: Body {
+                        id: Some(self.id),
+                        in_reply_to: input.body.id,
+                        payload: Payload::GenerateOk { guid },
+                    },
+                };
+                serde_json::to_writer(&mut *output, &reply)
+                    .context("serialize response to generate")?;
+                output.write_all(b"\n").context("write trailing newline")?;
+                self.id += 1;
+            }
+            Payload::GenerateOk { .. } => {}
+        }
+        Ok(())
+    }
+}
+
+fn main() -> anyhow::Result<()> {
+    main_loop(UniqueNode { id: 0 })
+}
